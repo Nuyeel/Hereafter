28d8671eb3c329cdfc925b938e799626a4d07f96
dandelion64 <coolpillow0308@gmail.com>
2022-07-17 21:47:44 +0800

    更新

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,28 @@
 import Background from './components/Background';
 import Nav from './components/Nav';
+
 // NextLife 目前只是測試引用 css 的位置
 // 統一放在 styles 中的好處是一目了然 集中管理
 // 大家的 css 都在同一個資料夾內
 // 可以自己先註解掉 只是讓大家知道可以用
-import NextLife from './pages/NextLife/NextLife';
+// import NextLife from './pages/NextLife/NextLife';
+
+import Genie from './components/Genie';
 
 function App() {
     return (
         <>
             <Nav />
-            <Background />
-            <NextLife />
-            <img src="http://localhost:3500/uploads/shared/test.png" alt="" />
+            {/* FIXME: Just For Test */}
+            {/* <Background theme="light" /> */}
+            <Background theme="dark" />
+            {/* <NextLife /> */}
+            {/* <img
+                src="http://localhost:3500/uploads/images/shared/test.png"
+                alt=""
+            /> */}
+            {/* FIXME: 目前 svg 素材會導致視窗被撐高 */}
+            {/* <Genie /> */}
         </>
     );
 }
