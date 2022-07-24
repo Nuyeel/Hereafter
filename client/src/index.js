1bb79a71b160a2275a8f219764d8bb95be304512
dandelion64 <coolpillow0308@gmail.com>
2022-07-24 22:59:24 +0800

    更新

--- client/src/index.js
+++ client/src/index.js
@@ -8,22 +8,22 @@ import App from './App';
 import './utils/onWindowResize';
 
 // 這里引入的 css 會套用至全域
-import './styles/reset.css';
-// 正式上線改用 bootstrap-grid.min.css
-import 'bootstrap/dist/css/bootstrap-grid.css';
+import './styles/reset.scss';
 // 正式上線改用 all.min.css
 import '@fortawesome/fontawesome-free/css/all.css';
 // 共通 css
-import './styles/index.css';
+import './styles/index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 // StrictMode 會 Render 兩次以檢查可能發生的錯誤
+// (Mount 並 UnMount 一次再重新 Mount 一次)
 // 所以 console.log() 也會印兩次
+// 為了方便除錯先註解掉
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    // <React.StrictMode>
+    <App />
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
