6c1698a8412066e92fb91080e7486b27cbf9539f
dandelion64 <coolpillow0308@gmail.com>
2022-07-24 22:53:27 +0800

    統一管理 AJAX 路徑

---
+++ client/src/config/ajax-path.js
@@ -0,0 +1,8 @@
+// 後端主機 Domain 和 Port
+export const SERVER = 'http://localhost:3500';
+
+// 分享牆部分
+export const API_SHAREWALL_TAGS = `${SERVER}/api/sharewall/tags`;
+
+// 測試部分 (之後可以刪除)
+export const TEST_LOGIN = `${SERVER}/test/member/login`;
