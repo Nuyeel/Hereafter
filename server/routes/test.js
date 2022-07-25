52c3665710e892e423527d24d42ef9fb6e0a5b62
dandelion64 <coolpillow0308@gmail.com>
2022-07-25 16:00:32 +0800

    新增測試登入用的路由

--- server/routes/test.js
+++ server/routes/test.js
@@ -29,6 +29,9 @@ const axios = require('axios');
 // 測試 JWT 用
 const jwt = require('jsonwebtoken');
 
+// 進行 bcrypt 加密用
+const bcryptjs = require('bcryptjs');
+
 // 路由管理
 // 測試: http://localhost:3500/test
 router
@@ -187,71 +190,84 @@ router.route('/ajax/axios/cwb').get((req, res) => {
 
 // 提供測試用的 JWT 登入登出路由
 // 測試: http://localhost:3500/test/member/login
-router.route('/member/login').get(async (req, res) => {
-    const sid = 1;
-    const account = 'HappyCat01';
-    // const password = 'HappyCat01';
+router.route('/member/login').post(async (req, res) => {
+    const output = {
+        code: 200,
+        success: false,
+        error: '',
+        data: {},
+    };
+
+    // 會員登入表格資料
+    // req.body.account = 'HappyCat01';
+    // req.body.password = 'HappyCat01';
 
     // 因為密碼的 hash 不是靠 PHP 生成了
-    // 所以這裡我沒有去驗證 畢竟密碼資料可能要改成用 bcryptjs 生出來
-    const $sql = ` SELECT * FROM member WHERE sid = ? AND account = ? `;
-    const [results] = await db.query($sql, [sid, account]);
+    // FIXME: 目前我只自行更動了 sid: 1 的 password hash
+    const $sql = ` SELECT * FROM member WHERE account = ? `;
+    const [result] = await db.query($sql, [req.body.account]);
 
-    // 如果存在該會員的話 (陣列存在且長度不為零)
-    if (results && results.length !== 0) {
-        // 這張表中的 sid 是 JWT 流水號 所以要改成用 member_sid
-        const $sql = ` SELECT COUNT(*) FROM admin_test_jwt WHERE member_sid = ${sid} `;
-        // 解構賦值同時改名
-        const [[{ 'COUNT(*)': resultRow }]] = await db.query($sql);
-
-        // 如果這名會員沒有 token 就簽署一個給他
-        if (resultRow === 0) {
-            const jwtPayload = {
-                id: sid,
-                account: account,
-                // 可以裝更多東西 看心情
-                // 不過最好不要太大
-                // FIXME: 目前測試表轉成 JSON 後最大長度是 255
-            };
-
-            const token = await jwt.sign(jwtPayload, process.env.JWT_SECRET);
-
-            // 然後將之寫入資料表中
-            const $sql =
-                'INSERT INTO `admin_test_jwt`(`member_sid`, `token`, `expires`, `payload`) VALUES (?, ?, ?, ?)';
-
-            await db.execute($sql, [
-                sid,
-                token,
-                // 用 JavaScript 當前毫秒數 + 20 分鐘存入
-                Date.now() + 1200000,
-                JSON.stringify(jwtPayload),
-            ]);
-        } else {
-            // 如果這個會員已經有 token 的話
-            // 延長其有效時間
-            const $sql = ' UPDATE `admin_test_jwt` SET `expires` = ? ';
-
-            await db.execute($sql, [Date.now()]);
-        }
-
-        // TODO: 存在該會員而且新增或修改了 token 後要傳回前端
-        // 只是筆記 不想找了
-        // jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
-        //     if (error) {
-        //         console.log({error});
-        //         // 解密失敗時會印出:
-        //         // JsonWebTokenError: invalid signature
-        //     }
-        //     console.log(decoded);
-        //     // case 1. 解密成功
-        //     // { id:12, account: 'shin', iat:1658301747 }
-        //     // case 2. 解密失敗
-        //     // undefined
-        //     process.exit();
-        // })
+    if (!result.length) {
+        // 不存在該會員或帳號打字錯誤
+        output.code = 401;
+        output.error = '該會員不存在';
+        return res.json(output);
     }
-    res.json(results);
+
+    if (!req.body.password) {
+        output.code = 401;
+        output.error = '請輸入密碼';
+        return res.json(output);
+    }
+
+    // result 是一個陣列 裏面只有一個物件 下行是為了取得物件
+    const row = result[0];
+    // TODO: 可以考慮修改資料表欄位名稱 password => pass_hash
+    output.success = await bcryptjs.compare(req.body.password, row.password);
+
+    if (!output.success) {
+        // 密碼錯誤 (密碼加密驗證失敗)
+        output.code = 401;
+        output.error = '密碼錯誤';
+        return res.json(output);
+    } else {
+        // 帳號密碼都對
+        // 這張表中的 sid 是 JWT 流水號 所以要改成用 member_sid
+        // 先把舊的 token 表資料整筆刪掉
+        const $del_sql = ' DELETE FROM `admin_test_jwt` WHERE member_sid = ? ';
+        await db.execute($del_sql, [row.sid]);
+
+        // 開始簽署新的 token 並寫入資料表
+        const jwtPayload = {
+            id: row.sid,
+            account: row.account,
+            // 可以裝更多東西 看心情
+            // 不過最好不要太大
+            // FIXME: 目前測試表轉成 JSON 後資料庫中最大長度是 255
+        };
+
+        const token = await jwt.sign(jwtPayload, process.env.JWT_SECRET);
+
+        // 然後將之寫入資料表中
+        const $sql =
+            'INSERT INTO `admin_test_jwt`(`member_sid`, `token`, `expires`, `payload`) VALUES (?, ?, ?, ?)';
+
+        await db.execute($sql, [
+            row.sid,
+            token,
+            // 用 JavaScript 當前毫秒數 + 20 分鐘存入
+            Date.now() + 1200000,
+            JSON.stringify(jwtPayload),
+        ]);
+
+        output.data = {
+            sid: row.sid,
+            token,
+            account: row.account,
+        };
+
+        return res.json(output);
+    }
 });
 
 // 輸出 router 這個 Middleware (Function)
