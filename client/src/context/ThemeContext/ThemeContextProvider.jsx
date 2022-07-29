eb035844943670587739206961903231d592c2f8
dandelion64 <coolpillow0308@gmail.com>
2022-07-29 23:50:54 +0800

    修正了傳出變數的方式與預設值

--- client/src/context/ThemeContext/ThemeContextProvider.jsx
+++ client/src/context/ThemeContext/ThemeContextProvider.jsx
@@ -4,14 +4,14 @@ import ThemeContext, { themes } from './ThemeContext';
 // ThemeContext 定義並傳出結構
 // ThemeContextProvider 設定 state 並傳出 Component
 function ThemeContextProvider(props) {
-    const [theme, setTheme] = useState(themes.dark);
+    const [theme, setTheme] = useState(themes.light);
 
     // 將方法解構賦值放在物件之中
     // 只有 value 的內容可以做為 Context 達成 drilling
     // 也可以給 ThemeProvider props
     // 但就一定要一層一層傳遞了
     return (
-        <ThemeContext.Provider value={{ ...theme, setTheme }}>
+        <ThemeContext.Provider value={{ theme, setTheme }}>
             {props.children}
         </ThemeContext.Provider>
     );
