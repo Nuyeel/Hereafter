cae870a593d5e6720685297456d3396fda5217a1
dandelion64 <coolpillow0308@gmail.com>
2022-07-29 23:49:34 +0800

    現在會根據主題變色

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,30 @@
 import { useContext } from 'react';
+
+import ThemeContext from '../context/ThemeContext/ThemeContext';
 import HeaderContext from '../context/HeaderContext/HeaderContext';
 
 function Header() {
     // TODO: headerList 中的 item.params 匹配時顯示 item.title
+    const { theme } = useContext(ThemeContext);
     const headerContext = useContext(HeaderContext);
 
     return (
         <div className="container d-flex mb-3">
             {headerContext.title && (
-                <h2 className="header">{headerContext.title}</h2>
+                <h2
+                    className="header"
+                    style={{
+                        color: theme.cHeader,
+                    }}
+                >
+                    {headerContext.title}
+                    <span
+                        className="header-baseline-decoration"
+                        style={{
+                            backgroundColor: theme.bgcHeaderDecor,
+                        }}
+                    />
+                </h2>
             )}
             {/* TODO: 某些頁面必須要顯示分頁鈕 例如分享牆 */}
             {/* <button style={{ height: '30px' }}>fddsfdfds</button> */}
