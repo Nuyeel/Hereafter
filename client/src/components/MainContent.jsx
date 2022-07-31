cc65ba3d16ff4b099ddd2699a4d190ff6825e3fe
dandelion64 <coolpillow0308@gmail.com>
2022-07-31 22:13:23 +0800

    padding-top 改用 className 管理以配合 Nav 的高度 RWD

--- client/src/components/MainContent.jsx
+++ client/src/components/MainContent.jsx
@@ -8,8 +8,6 @@ import Header from './Header';
 // DONE: 改用 index.scss 搭配 className 管理
 
 function MainContent(props) {
-    const { theme } = useContext(ThemeContext);
-
     return (
         // 感覺以我們的設計來說不用 <main> 但先維持
         // 各子區域 margin 最好統一是 bottom 來防止 margin collapsing
