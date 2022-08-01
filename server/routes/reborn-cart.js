5ec20455f24487abb48023aecc323573db133c22
Chuan Min <chuanmin13@gmail.com>
2022-08-01 14:17:50 +0800

    更新 良辰吉地 & 轉生購物車 api

--- server/routes/reborn-cart.js
+++ server/routes/reborn-cart.js
@@ -26,7 +26,7 @@ const getPlaceInCart = async (member) => {
 
 // CRUD
 router.post('/', async (req, res) => {
-    // body: place_sid, member_sid??
+    // body: place_sid, member_sid
     const output = {
         success: false,
         error: '',
@@ -34,10 +34,21 @@ router.post('/', async (req, res) => {
 
     // 判斷是否在購物車
     const sql = `SELECT COUNT(1) num FROM place_in_cart WHERE place_sid=? AND member_sid=?`;
-    const [[{ num }]] = await db.query(sql, [req.body.place_sid, fake_member]);
+    const [[{ num }]] = await db.query(sql, [
+        req.body.place_sid,
+        req.body.member_sid,
+    ]);
     if (num > 0) {
         output.error = '購物車已有此項商品';
         return res.json(output);
+    }
+    //判斷購物車是否達5筆資料上限
+    const sql4 = `SELECT COUNT(1) total FROM place_in_cart WHERE member_sid=?`;
+    const [[{ total }]] = await db.query(sql4, [req.body.member_sid]);
+    if (total === 5) {
+        output.error = '購物車已滿';
+        return res.json(output);
+        // TODO: 前台react提醒
     }
 
     if (!req.body.place_sid) {
@@ -55,12 +66,15 @@ router.post('/', async (req, res) => {
     const sql3 = `INSERT INTO place_in_cart (member_sid, place_sid) VALUES (?, ?)`;
 
     // 假設會員編號為10
-    const [r3] = await db.query(sql3, [10, req.body.place_sid]);
+    const [r3] = await db.query(sql3, [
+        req.body.member_sid,
+        req.body.place_sid,
+    ]);
     if (r3.affectedRows) {
         output.success = true;
     }
 
-    output.member_cart = await getPlaceInCart(fake_member);
+    output.member_cart = await getPlaceInCart(req.body.member_sid);
     res.json(output);
 });
 
@@ -68,12 +82,13 @@ router.get('/:memberSid', async (req, res) => {
     res.json(await getPlaceInCart(req.params.memberSid));
 });
 
+// 會員將資料移出購物車
 router.delete('/', async (req, res) => {
     // place_sid
     const sql = `DELETE FROM place_in_cart WHERE place_sid=? AND member_sid=?`;
-    await db.query(sql, [req.body.place_sid, fake_member]);
+    await db.query(sql, [req.body.place_sid, req.body.member_sid]);
 
-    res.json(await getPlaceInCart(fake_member));
+    res.json(await getPlaceInCart(req.body.member_sid));
 });
 
 module.exports = router;
