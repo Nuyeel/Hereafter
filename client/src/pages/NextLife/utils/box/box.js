1e3e67b8e89e4229a592a97de677a928439739a9
dandelion64 <coolpillow0308@gmail.com>
2022-08-03 22:57:20 +0800

    將提供必要資料的部分切分出去 避免主體過於雜亂

---
+++ client/src/pages/NextLife/utils/box/box.js
@@ -0,0 +1,394 @@
+// DONE: 用來 import/export 所有方塊表面素材
+
+// MATERIALS: 1 (SKIPPED)
+
+// TEXTURE: Box 26
+// import _26T from '../../../../images/NextLifeCube/box/26_t.png';
+
+// TEXTURE: Box 29
+// import _29T from '../../../../images/NextLifeCube/box/29_t.png';
+
+// TEXTURE: Box 33
+// import _33T from '../../../../images/NextLifeCube/box/33_t.png';
+
+// TEXTURE: Box 36
+// import _36T from '../../../../images/NextLifeCube/box/36_t.png';
+
+// MATERIALS: 2
+
+// TEXTURE: Box 27
+import _27A from '../../../../images/NextLifeCube/box/27_a.png';
+import _27T from '../../../../images/NextLifeCube/box/27_t.png';
+
+// MATERIALS: 3
+
+// TEXTURE: Box 0
+import _0A from '../../../../images/NextLifeCube/box/0_a.png';
+import _0B from '../../../../images/NextLifeCube/box/0_b.png';
+import _0T from '../../../../images/NextLifeCube/box/0_t.png';
+
+// TEXTURE: Box 1
+import _1A from '../../../../images/NextLifeCube/box/1_a.png';
+import _1B from '../../../../images/NextLifeCube/box/1_b.png';
+import _1T from '../../../../images/NextLifeCube/box/1_t.png';
+
+// TEXTURE: Box 6
+import _6A from '../../../../images/NextLifeCube/box/6_a.png';
+import _6B from '../../../../images/NextLifeCube/box/6_b.png';
+import _6T from '../../../../images/NextLifeCube/box/6_t.png';
+
+// TEXTURE: Box 15
+import _15A from '../../../../images/NextLifeCube/box/15_a.png';
+import _15B from '../../../../images/NextLifeCube/box/15_b.png';
+import _15T from '../../../../images/NextLifeCube/box/15_t.png';
+
+// TEXTURE: Box 20
+import _20A from '../../../../images/NextLifeCube/box/20_a.png';
+import _20B from '../../../../images/NextLifeCube/box/20_b.png';
+import _20T from '../../../../images/NextLifeCube/box/20_t.png';
+
+// TEXTURE: Box 22
+import _22A from '../../../../images/NextLifeCube/box/22_a.png';
+import _22B from '../../../../images/NextLifeCube/box/22_b.png';
+import _22T from '../../../../images/NextLifeCube/box/22_t.png';
+
+// TEXTURE: Box 23
+import _23A from '../../../../images/NextLifeCube/box/23_a.png';
+import _23B from '../../../../images/NextLifeCube/box/23_b.png';
+import _23T from '../../../../images/NextLifeCube/box/23_t.png';
+
+// TEXTURE: Box 25
+import _25A from '../../../../images/NextLifeCube/box/25_a.png';
+import _25B from '../../../../images/NextLifeCube/box/25_b.png';
+import _25T from '../../../../images/NextLifeCube/box/25_t.png';
+
+// TEXTURE: Box 30
+import _30A from '../../../../images/NextLifeCube/box/30_a.png';
+import _30B from '../../../../images/NextLifeCube/box/30_b.png';
+import _30T from '../../../../images/NextLifeCube/box/30_t.png';
+
+// MATERIALS: 4
+
+// TEXTURE: Box 2
+import _2A from '../../../../images/NextLifeCube/box/2_a.png';
+import _2B from '../../../../images/NextLifeCube/box/2_b.png';
+import _2C from '../../../../images/NextLifeCube/box/2_b.png';
+import _2T from '../../../../images/NextLifeCube/box/2_t.png';
+
+// TEXTURE: Box 3
+import _3A from '../../../../images/NextLifeCube/box/3_a.png';
+import _3B from '../../../../images/NextLifeCube/box/3_b.png';
+import _3C from '../../../../images/NextLifeCube/box/3_b.png';
+import _3T from '../../../../images/NextLifeCube/box/3_t.png';
+
+// TEXTURE: Box 4
+import _4A from '../../../../images/NextLifeCube/box/4_a.png';
+import _4B from '../../../../images/NextLifeCube/box/4_b.png';
+import _4C from '../../../../images/NextLifeCube/box/4_b.png';
+import _4T from '../../../../images/NextLifeCube/box/4_t.png';
+
+// TEXTURE: Box 5
+import _5A from '../../../../images/NextLifeCube/box/5_a.png';
+import _5B from '../../../../images/NextLifeCube/box/5_b.png';
+import _5C from '../../../../images/NextLifeCube/box/5_b.png';
+import _5T from '../../../../images/NextLifeCube/box/5_t.png';
+
+// TEXTURE: Box 7
+import _7A from '../../../../images/NextLifeCube/box/7_a.png';
+import _7B from '../../../../images/NextLifeCube/box/7_b.png';
+import _7C from '../../../../images/NextLifeCube/box/7_b.png';
+import _7T from '../../../../images/NextLifeCube/box/7_t.png';
+
+// TEXTURE: Box 8
+import _8A from '../../../../images/NextLifeCube/box/8_a.png';
+import _8B from '../../../../images/NextLifeCube/box/8_b.png';
+import _8C from '../../../../images/NextLifeCube/box/8_b.png';
+import _8T from '../../../../images/NextLifeCube/box/8_t.png';
+
+// TEXTURE: Box 9
+import _9A from '../../../../images/NextLifeCube/box/9_a.png';
+import _9B from '../../../../images/NextLifeCube/box/9_b.png';
+import _9C from '../../../../images/NextLifeCube/box/9_b.png';
+import _9T from '../../../../images/NextLifeCube/box/9_t.png';
+
+// TEXTURE: Box 10
+import _10A from '../../../../images/NextLifeCube/box/10_a.png';
+import _10B from '../../../../images/NextLifeCube/box/10_b.png';
+import _10C from '../../../../images/NextLifeCube/box/10_b.png';
+import _10T from '../../../../images/NextLifeCube/box/10_t.png';
+
+// TEXTURE: Box 11
+import _11A from '../../../../images/NextLifeCube/box/11_a.png';
+import _11B from '../../../../images/NextLifeCube/box/11_b.png';
+import _11C from '../../../../images/NextLifeCube/box/11_b.png';
+import _11T from '../../../../images/NextLifeCube/box/11_t.png';
+
+// TEXTURE: Box 12
+import _12A from '../../../../images/NextLifeCube/box/12_a.png';
+import _12B from '../../../../images/NextLifeCube/box/12_b.png';
+import _12C from '../../../../images/NextLifeCube/box/12_b.png';
+import _12T from '../../../../images/NextLifeCube/box/12_t.png';
+
+// TEXTURE: Box 13
+import _13A from '../../../../images/NextLifeCube/box/13_a.png';
+import _13B from '../../../../images/NextLifeCube/box/13_b.png';
+import _13C from '../../../../images/NextLifeCube/box/13_b.png';
+import _13T from '../../../../images/NextLifeCube/box/13_t.png';
+
+// TEXTURE: Box 14
+import _14A from '../../../../images/NextLifeCube/box/14_a.png';
+import _14B from '../../../../images/NextLifeCube/box/14_b.png';
+import _14C from '../../../../images/NextLifeCube/box/14_b.png';
+import _14T from '../../../../images/NextLifeCube/box/14_t.png';
+
+// TEXTURE: Box 16
+import _16A from '../../../../images/NextLifeCube/box/16_a.png';
+import _16B from '../../../../images/NextLifeCube/box/16_b.png';
+import _16C from '../../../../images/NextLifeCube/box/16_b.png';
+import _16T from '../../../../images/NextLifeCube/box/16_t.png';
+
+// TEXTURE: Box 17
+import _17A from '../../../../images/NextLifeCube/box/17_a.png';
+import _17B from '../../../../images/NextLifeCube/box/17_b.png';
+import _17C from '../../../../images/NextLifeCube/box/17_b.png';
+import _17T from '../../../../images/NextLifeCube/box/17_t.png';
+
+// TEXTURE: Box 18
+import _18A from '../../../../images/NextLifeCube/box/18_a.png';
+import _18B from '../../../../images/NextLifeCube/box/18_b.png';
+import _18C from '../../../../images/NextLifeCube/box/18_b.png';
+import _18T from '../../../../images/NextLifeCube/box/18_t.png';
+
+// TEXTURE: Box 19
+import _19A from '../../../../images/NextLifeCube/box/19_a.png';
+import _19B from '../../../../images/NextLifeCube/box/19_b.png';
+import _19C from '../../../../images/NextLifeCube/box/19_b.png';
+import _19T from '../../../../images/NextLifeCube/box/19_t.png';
+
+// TEXTURE: Box 28
+import _28A from '../../../../images/NextLifeCube/box/28_a.png';
+import _28B from '../../../../images/NextLifeCube/box/28_b.png';
+import _28C from '../../../../images/NextLifeCube/box/28_b.png';
+import _28T from '../../../../images/NextLifeCube/box/28_t.png';
+
+// TEXTURE: Box 31
+import _31A from '../../../../images/NextLifeCube/box/31_a.png';
+import _31B from '../../../../images/NextLifeCube/box/31_b.png';
+import _31C from '../../../../images/NextLifeCube/box/31_b.png';
+import _31T from '../../../../images/NextLifeCube/box/31_t.png';
+
+// TEXTURE: Box 32
+import _32A from '../../../../images/NextLifeCube/box/32_a.png';
+import _32B from '../../../../images/NextLifeCube/box/32_b.png';
+import _32C from '../../../../images/NextLifeCube/box/32_b.png';
+import _32T from '../../../../images/NextLifeCube/box/32_t.png';
+
+// TEXTURE: Box 37
+import _37A from '../../../../images/NextLifeCube/box/37_a.png';
+import _37B from '../../../../images/NextLifeCube/box/37_b.png';
+import _37C from '../../../../images/NextLifeCube/box/37_b.png';
+import _37T from '../../../../images/NextLifeCube/box/37_t.png';
+
+// MATERIALS: 5
+
+// TEXTURE: Box 21
+import _21A from '../../../../images/NextLifeCube/box/21_a.png';
+import _21B from '../../../../images/NextLifeCube/box/21_b.png';
+import _21C from '../../../../images/NextLifeCube/box/21_b.png';
+import _21D from '../../../../images/NextLifeCube/box/21_d.png';
+import _21T from '../../../../images/NextLifeCube/box/21_t.png';
+
+// TEXTURE: Box 24
+import _24A from '../../../../images/NextLifeCube/box/24_a.png';
+import _24B from '../../../../images/NextLifeCube/box/24_b.png';
+import _24C from '../../../../images/NextLifeCube/box/24_b.png';
+import _24D from '../../../../images/NextLifeCube/box/24_d.png';
+import _24T from '../../../../images/NextLifeCube/box/24_t.png';
+
+// TEXTURE: Box 34
+import _34A from '../../../../images/NextLifeCube/box/34_a.png';
+import _34B from '../../../../images/NextLifeCube/box/34_b.png';
+import _34C from '../../../../images/NextLifeCube/box/34_b.png';
+import _34D from '../../../../images/NextLifeCube/box/34_d.png';
+import _34T from '../../../../images/NextLifeCube/box/34_t.png';
+
+// TEXTURE: Box 35
+import _35A from '../../../../images/NextLifeCube/box/35_a.png';
+import _35B from '../../../../images/NextLifeCube/box/35_b.png';
+import _35C from '../../../../images/NextLifeCube/box/35_b.png';
+import _35D from '../../../../images/NextLifeCube/box/35_d.png';
+import _35T from '../../../../images/NextLifeCube/box/35_t.png';
+
+export {
+    // Box 0
+    _0A,
+    _0B,
+    _0T,
+    // Box 1
+    _1A,
+    _1B,
+    _1T,
+    // Box 2
+    _2A,
+    _2B,
+    _2C,
+    _2T,
+    // Box 3
+    _3A,
+    _3B,
+    _3C,
+    _3T,
+    // Box 4
+    _4A,
+    _4B,
+    _4C,
+    _4T,
+    // Box 5
+    _5A,
+    _5B,
+    _5C,
+    _5T,
+    // Box 6
+    _6A,
+    _6B,
+    _6T,
+    // Box 7
+    _7A,
+    _7B,
+    _7C,
+    _7T,
+    // Box 8
+    _8A,
+    _8B,
+    _8C,
+    _8T,
+    // Box 9
+    _9A,
+    _9B,
+    _9C,
+    _9T,
+    // Box 10
+    _10A,
+    _10B,
+    _10C,
+    _10T,
+    // Box 11
+    _11A,
+    _11B,
+    _11C,
+    _11T,
+    // Box 12
+    _12A,
+    _12B,
+    _12C,
+    _12T,
+    // Box 13
+    _13A,
+    _13B,
+    _13C,
+    _13T,
+    // Box 14
+    _14A,
+    _14B,
+    _14C,
+    _14T,
+    // Box 15
+    _15A,
+    _15B,
+    _15T,
+    // Box 16
+    _16A,
+    _16B,
+    _16C,
+    _16T,
+    // Box 17
+    _17A,
+    _17B,
+    _17C,
+    _17T,
+    // Box 18
+    _18A,
+    _18B,
+    _18C,
+    _18T,
+    // Box 19
+    _19A,
+    _19B,
+    _19C,
+    _19T,
+    // Box 20
+    _20A,
+    _20B,
+    _20T,
+    // Box 21
+    _21A,
+    _21B,
+    _21C,
+    _21D,
+    _21T,
+    // Box 22
+    _22A,
+    _22B,
+    _22T,
+    // Box 23
+    _23A,
+    _23B,
+    _23T,
+    // Box 24
+    _24A,
+    _24B,
+    _24C,
+    _24D,
+    _24T,
+    // Box 25
+    _25A,
+    _25B,
+    _25T,
+    // Box 26
+    // FIXME: SKIPPED
+    // Box 27
+    _27A,
+    _27T,
+    // Box 28
+    _28A,
+    _28B,
+    _28C,
+    _28T,
+    // Box 29
+    // FIXME: SKIPPED
+    // Box 30
+    _30A,
+    _30B,
+    _30T,
+    // Box 31
+    _31A,
+    _31B,
+    _31C,
+    _31T,
+    // Box 32
+    _32A,
+    _32B,
+    _32C,
+    _32T,
+    // Box 33
+    // FIXME: SKIPPED
+    // Box 34
+    _34A,
+    _34B,
+    _34C,
+    _34D,
+    _34T,
+    // Box 35
+    _35A,
+    _35B,
+    _35C,
+    _35D,
+    _35T,
+    // Box 36
+    // FIXME: SKIPPED
+    // Box 37
+    _37A,
+    _37B,
+    _37C,
+    _37T,
+};
