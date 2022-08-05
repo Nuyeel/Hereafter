4ac83243cc1288d5c07055a45b19dd4f690e6ea1
hsin007 <a2827234@gmail.com>
2022-08-05 11:12:23 +0800

    Fields checking

--- client/src/pages/Register/RegisterForm.jsx
+++ client/src/pages/Register/RegisterForm.jsx
@@ -38,8 +38,19 @@ function RegisterForm(props) {
     const [accountSearch, setAccountSearch] = useState('');
     const [emailSearch, setEmailSearch] = useState('');
     const [passwordSearch, setPasswordSearch] = useState('');
+    const [confrimPasswordSearch, setConfirmPasswordSearch] = useState('');
     const [validationCssClassname, setValidationCssClassname] = useState('');
+    const [validationCssClassname2, setValidationCssClassname2] = useState('');
+    const [validationCssClassname3, setValidationCssClassname3] = useState('');
+    const [validationCssClassname4, setValidationCssClassname4] = useState('');
 
+    // 正規表達式定義在這邊，前端檢查only
+    const accountRe = /^[a-zA-Z0-9_]\w*$/;
+    const emailRe =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zAZ]{2,}))$/;
+    const passwordRe = /^[a-zA-Z0-9_]\w*.{6,}$/;
+
+    // 帳號檢查
     const handleAccountSearch = (searchAccountWord) => {
         // 初始狀態設為空
         if (!searchAccountWord) {
@@ -47,37 +58,128 @@ function RegisterForm(props) {
             return;
         }
         // // 即時驗證欄位條件
-        if (searchAccountWord.length <= 10 && searchAccountWord.length >= 6) {
+        if (
+            searchAccountWord.length <= 10 &&
+            searchAccountWord.length >= 3 &&
+            searchAccountWord.match(accountRe)
+        ) {
             setValidationCssClassname('is-valid');
         } else {
             setValidationCssClassname('is-invalid');
         }
     };
 
-    // 400毫秒後做欄位檢查
-    const debounceHandleSearch = useCallback(
-        _.debounce(handleAccountSearch, 400),
+    // 設定300毫秒後做欄位檢查
+    const debounceHandleSearch1 = useCallback(
+        _.debounce(handleAccountSearch, 300),
         []
     );
 
-    const handleChange = (e) => {
+    const handleAccountChange = (e) => {
         // 可控元件綁用state使用
         setAccountSearch(e.target.value);
         // 搜尋用 - trim去除空白，toLowerCase轉小寫英文
         const searchAccountWord = e.target.value.trim().toLowerCase();
         // 傳至debounceFn中
-        debounceHandleSearch(searchAccountWord);
+        debounceHandleSearch1(searchAccountWord);
     };
 
+    // 電子信箱檢查
+    const handleEmailSearch = (searchEmailWord) => {
+        // 初始狀態設為空
+        if (!searchEmailWord) {
+            setValidationCssClassname2('');
+            return;
+        }
+        // // 即時驗證欄位條件
+        if (searchEmailWord.match(emailRe)) {
+            setValidationCssClassname2('is-valid');
+        } else {
+            setValidationCssClassname2('is-invalid');
+        }
+    };
+
+    // 設定300毫秒後做欄位檢查
+    const debounceHandleSearch2 = useCallback(
+        _.debounce(handleEmailSearch, 300),
+        []
+    );
+
+    const handleEmailChange = (e) => {
+        // 可控元件綁用state使用
+        setEmailSearch(e.target.value);
+        // 搜尋用 - trim去除空白，toLowerCase轉小寫英文
+        const searchEmailWord = e.target.value.trim().toLowerCase();
+        // 傳至debounceFn中
+        debounceHandleSearch2(searchEmailWord);
+    };
+
+    // 密碼檢查
+    const handlePasswordSearch = (searchPasswordWord) => {
+        // 初始狀態設為空
+        if (!searchPasswordWord) {
+            setValidationCssClassname3('');
+            return;
+        }
+        // // 即時驗證欄位條件
+        if (searchPasswordWord.match(passwordRe)) {
+            setValidationCssClassname3('is-valid');
+        } else {
+            setValidationCssClassname3('is-invalid');
+        }
+    };
+
+    // 設定300毫秒後做欄位檢查
+    const debounceHandleSearch3 = useCallback(
+        _.debounce(handlePasswordSearch, 300),
+        []
+    );
+
+    const handlePasswordChange = (e) => {
+        // 可控元件綁用state使用
+        setPasswordSearch(e.target.value);
+        // 搜尋用 - trim去除空白，toLowerCase轉小寫英文
+        const searchPasswordWord = e.target.value.trim().toLowerCase();
+        // 傳至debounceFn中
+        debounceHandleSearch3(searchPasswordWord);
+    };
+
+    // 確認密碼檢查
+    const handleConfirmPasswordSearch = (searchConfirmPasswordWord) => {
+        // 初始狀態設為空
+        if (!searchConfirmPasswordWord) {
+            setValidationCssClassname4('');
+            return;
+        }
+        console.log(searchConfirmPasswordWord);
+        console.log(registerData.password);
+        // // 即時驗證欄位條件
+        if (searchConfirmPasswordWord === registerData.password) {
+            setValidationCssClassname4('is-valid');
+        } else {
+            setValidationCssClassname4('is-invalid');
+        }
+    };
+
+    // 設定300毫秒後做欄位檢查
+    const debounceHandleSearch4 = useCallback(
+        _.debounce(handleConfirmPasswordSearch, 300),
+        []
+    );
+
+    const handleConfirmPasswordChange = (e) => {
+        // 可控元件綁用state使用
+        setConfirmPasswordSearch(e.target.value);
+        // 搜尋用 - trim去除空白，toLowerCase轉小寫英文
+        const searchConfirmPasswordWord = e.target.value.trim().toLowerCase();
+        // 傳至debounceFn中
+        debounceHandleSearch4(searchConfirmPasswordWord);
+    };
+
+    // 送出表單的部分
     const handleSubmit = async (e) => {
         e.preventDefault();
         // TODO: 欄位檢查
-
-        // // 正規表達式定義在這邊，為方便開發可都先註解
-        // const accountRe = /^[a-zA-Z0-9_]\w*$/;
-        // // const emailRe =
-        // //     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zAZ]{2,}))$/;
-        // const passwordRe = /^[a-zA-Z0-9_]\w*.{8,}$/;
 
         // if (registerData.account.length > 10) {
         //     alert('您設定的帳戶字數過長');
@@ -98,6 +200,7 @@ function RegisterForm(props) {
         //     alert('密碼與確認密碼需要一致');
         //     return;
         // }
+
 
         fetch(MEMBER_REGISTER, {
             method: 'POST',
@@ -151,8 +254,10 @@ function RegisterForm(props) {
                                                         type="text"
                                                         className={`form-control ${validationCssClassname}`}
                                                         name="account"
-                                                        placeholder="請建立一個登入帳戶"
-                                                        onChange={handleChange}
+                                                        placeholder="例：HappyCats"
+                                                        onChange={
+                                                            handleAccountChange
+                                                        }
                                                         maxLength="10"
                                                         required
                                                     />
@@ -164,16 +269,13 @@ function RegisterForm(props) {
                                                     >
                                                         電子信箱
                                                     </label>
-                                                    <input
+                                                    <InputIME
                                                         type="email"
-                                                        className="form-control"
+                                                        className={`form-control ${validationCssClassname2}`}
                                                         name="email"
-                                                        placeholder="請輸入一個有效信箱"
-                                                        value={
-                                                            registerData.email
-                                                        }
+                                                        placeholder="請輸入一個有效的電子信箱"
                                                         onChange={
-                                                            handleFieldsChange
+                                                            handleEmailChange
                                                         }
                                                         required
                                                     />
@@ -185,16 +287,13 @@ function RegisterForm(props) {
                                                     >
                                                         登入密碼
                                                     </label>
-                                                    <input
+                                                    <InputIME
                                                         type={passwordFieldType}
-                                                        className="form-control"
+                                                        className={`form-control ${validationCssClassname3}`}
                                                         name="password"
-                                                        placeholder="請建立一個密碼"
-                                                        value={
-                                                            registerData.password
-                                                        }
+                                                        placeholder="英文大小寫及數字至少七字"
                                                         onChange={
-                                                            handleFieldsChange
+                                                            handlePasswordChange
                                                         }
                                                         required
                                                     />
@@ -206,18 +305,15 @@ function RegisterForm(props) {
                                                     >
                                                         重新輸入密碼
                                                     </label>
-                                                    <input
+                                                    <InputIME
                                                         type={
                                                             confirmPasswordFieldType
                                                         }
-                                                        className="form-control"
+                                                        className={`form-control ${validationCssClassname4}`}
                                                         name="confirmPassword"
-                                                        placeholder="再輸入一次密碼"
-                                                        value={
-                                                            registerData.confirmPassword
-                                                        }
+                                                        placeholder="請再輸入一次前一欄的密碼"
                                                         onChange={
-                                                            handleFieldsChange
+                                                            handleConfirmPasswordChange
                                                         }
                                                         required
                                                     />
