7e9870102d02ee9406dcc1176768f69be6e8b3ae
dandelion64 <coolpillow0308@gmail.com>
2022-08-07 23:23:49 +0800

    新增了分享牆用的 Header Button 需要的狀態及方法

--- client/src/context/HeaderContext/HeaderContextProvider.jsx
+++ client/src/context/HeaderContext/HeaderContextProvider.jsx
@@ -4,8 +4,23 @@ import HeaderContext, { headers } from './HeaderContext';
 function HeaderContextProvider(props) {
     const [header, setHeader] = useState(headers.default);
 
+    // DONE: 分享牆分頁按鈕使用的功能
+    const [shareWallPostsData, setShareWallPostsData] = useState([]);
+    // TODO: 三態 'default', 'isAuthor', 'isCollector'
+
+    const [shareWallSearchState, setShareWallSearchState] = useState('default');
+
     return (
-        <HeaderContext.Provider value={{ ...header, setHeader }}>
+        <HeaderContext.Provider
+            value={{
+                ...header,
+                setHeader,
+                shareWallPostsData,
+                setShareWallPostsData,
+                shareWallSearchState,
+                setShareWallSearchState,
+            }}
+        >
             {props.children}
         </HeaderContext.Provider>
     );
