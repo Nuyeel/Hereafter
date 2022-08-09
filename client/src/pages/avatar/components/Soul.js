e6f03b7691e8b2b1b900d585105067a5be70b604
Mou <akimouin@gmail.com>
2022-08-09 19:09:21 +0800

    換色鬼鬼微調

--- client/src/pages/avatar/components/Soul.js
+++ client/src/pages/avatar/components/Soul.js
@@ -1,22 +1,23 @@
 import { ReactComponent as SoulSVG } from '../../../images/Nav/nav_soul.svg';
-import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import { useContext } from 'react';
 import ThemeContext from '../../../context/ThemeContext/ThemeContext';
 
 function Soul() {
     const { theme } = useContext(ThemeContext);
+    const Soulcolor = styled.i`
+        path {
+            fill: ${theme.cHeader};
+        }
+        circle {
+            stroke: ${theme.cHeader};
+        }
+    `;
     return (
         <>
-            <SoulSVG
-                css={css`
-                    path {
-                        fill: ${theme.cHeader};
-                    }
-                    circle {
-                        stroke: ${theme.cHeader};
-                    }
-                `}
-            />
+            <Soulcolor>
+                <SoulSVG />
+            </Soulcolor>
         </>
     );
 }
