df1b7bbe562a71b5ab2f79453058db2a4d9b1c65
Abby <chiquzt@gmail.com>
2022-08-10 22:49:52 +0800

    圖片

--- client/src/pages/News/News.js
+++ client/src/pages/News/News.js
@@ -1,14 +1,33 @@
+// import { useContext, useEffect } from 'react';
+
 import NewsCard from './components/NewsCard';
 import './StyleNews.scss';
+import newsData from './data/newsData.json';
+// import PopCard from './components/PopCard';
+
 function News() {
+    // const [newsData, setNewsData] = useState([]);
+
+    // useEffect(() => {
+    //     //axios
+
+    //     setNewsData(newsDatas);
+    // }, []);
+
     return (
         <>
             <div className="container">
                 <div className="header">最新消息</div>
-                <div className="ab-bg">
-                    <div className="ab-news-box">
-                        <div className="ab-news-card-wrap">
-                            <NewsCard />
+                <div className="ab-bg-wrap">
+                    {/*<PopCard />*/}
+                    <div className="ab-news-bg">
+                        <div className="ab-news-box">
+                            <div className="ab-news-card-wrap">
+                                <div className="ab-fake"></div>
+                                {newsData.news.map((v, i) => {
+                                    return <NewsCard key={v.sid} card={v} />;
+                                })}
+                            </div>
                         </div>
                     </div>
                 </div>
