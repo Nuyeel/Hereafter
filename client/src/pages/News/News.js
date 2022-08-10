3c9c263f12d31b66daf639d92bf836ebd2202c20
Abby <chiquzt@gmail.com>
2022-08-10 23:29:20 +0800

    news

--- client/src/pages/News/News.js
+++ client/src/pages/News/News.js
@@ -3,7 +3,7 @@
 import NewsCard from './components/NewsCard';
 import './StyleNews.scss';
 import newsData from './data/newsData.json';
-// import PopCard from './components/PopCard';
+import PopCard from './components/PopCard';
 
 function News() {
     // const [newsData, setNewsData] = useState([]);
@@ -13,19 +13,28 @@ function News() {
 
     //     setNewsData(newsDatas);
     // }, []);
+    const handleClick = () => {
+        console.log('123');
+    };
 
     return (
         <>
             <div className="container">
                 <div className="header">最新消息</div>
                 <div className="ab-bg-wrap">
-                    {/*<PopCard />*/}
+                    <PopCard />
                     <div className="ab-news-bg">
                         <div className="ab-news-box">
                             <div className="ab-news-card-wrap">
                                 <div className="ab-fake"></div>
                                 {newsData.news.map((v, i) => {
-                                    return <NewsCard key={v.sid} card={v} />;
+                                    return (
+                                        <NewsCard
+                                            onClick={handleClick}
+                                            key={v.sid}
+                                            card={v}
+                                        />
+                                    );
                                 })}
                             </div>
                         </div>
