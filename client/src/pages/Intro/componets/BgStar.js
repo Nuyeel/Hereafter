8f99269aafdd346751cf425565fa0866ae4ebb2d
NAOMI LEE <christinalee22l4@gmail.com>
2022-08-11 15:41:40 +0800

    intro animation

---
+++ client/src/pages/Intro/componets/BgStar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function BgStar() {
+  return (
+    <>
+    <div className="otherStars">
+                            <div className="otherStar"></div>
+                            <div className="otherStar"></div>
+                            <div className="otherStar"></div>
+                            <div className="otherStar"></div>
+                            <div className="otherStar"></div>
+                            <div className="otherStar"></div>
+                            <div className="otherStar"></div>
+                        </div>
+    </>
+  )
+}
+
+export default BgStar
