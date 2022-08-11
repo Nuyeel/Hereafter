57dcbbc4e1f095c93c2f1aae4f13c4a263f4fbca
Chuan Min <chuanmin13@gmail.com>
2022-08-11 14:29:04 +0800

    fix bug

--- client/src/pages/Place/components/util/addPlaceToCart.js
+++ client/src/pages/Place/components/util/addPlaceToCart.js
@@ -1,29 +1,25 @@
 import { PLACE_CARTDATA_API } from '../../../../config/ajax-path';
 
-function AddPlaceToCart(props) {
-    const { e, userSid, isDead } = props;
+function AddPlaceToCart(e, userSid) {
+    const placeIndex = e.currentTarget
+        .closest('.place-info-card')
+        .getAttribute('data-placesid');
+    console.log(placeIndex, userSid);
 
-    if (isDead && userSid) {
-        const placeIndex = e.currentTarget
-            .closest('.place-info-card')
-            .getAttribute('data-placesid');
-        console.log(placeIndex, userSid);
+    // 存到資料庫
+    const obj = { member_sid: userSid, place_sid: placeIndex };
 
-        // 存到資料庫
-        const obj = { member_sid: userSid, place_sid: placeIndex };
-
-        fetch(PLACE_CARTDATA_API, {
-            method: 'POST',
-            body: JSON.stringify(obj),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then((r) => r.json())
-            .then((result) => {
-                console.log(result);
-            });
-    }
+    fetch(PLACE_CARTDATA_API, {
+        method: 'POST',
+        body: JSON.stringify(obj),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((r) => r.json())
+        .then((result) => {
+            console.log(result);
+        });
 
     return;
 }
