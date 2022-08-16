e692bcbc5e2288bd46a2036a56c74df17d927dcb
NAOMI LEE <christinalee22l4@gmail.com>
2022-08-16 12:11:25 +0800

    動畫修正

--- client/src/pages/Intro/index.js
+++ client/src/pages/Intro/index.js
@@ -2,7 +2,9 @@
 import React from 'react';
 import './introstyle.scss';
 
-import Clock from './componets/Clock';
+// import Clock from './componets/Clock';
+import Clock2 from './componets/Clock2';
+
 import Earth from './componets/Earth';
 import BgStar from './componets/BgStar';
 import { useNavigate } from 'react-router-dom';
@@ -44,8 +46,11 @@ function Intro() {
                 // `p` is number (0~1)
                 // When just before this page appear, `p` will be 0
                 // When this page filled your screen, `p` will be 1
-                transform: (p) => `rotate(${-(10 + p * 50)}deg)`,
-                transformOrigin: 'top center  ',
+                transformOrigin: `right top`,
+                transform: (p) =>
+                    `
+                    
+                    rotate(${-(20 + p * 50)}deg)`,
             },
         },
         out: {
@@ -54,9 +59,51 @@ function Intro() {
                 // When this page filled your screen, `p` will be 0
                 // When just after this page disappear, `p` will be 1
                 // transform: (p) => `rotate(${(50 + p * 40)}deg)`,
-                transform: `rotate (40deg)`,
+                transformOrigin: `right top`,
+                transform: (p) => `
+              
+                 rotate (${50 - p * 50}deg)`,
+            },
+        },
+    });
+    const FadeStay = () => ({
+        in: {
+            style: {
+                opacity: (p) => 0 * p,
+            },
+        },
+        out: {
+            style: {
+                opacity: (p) => 3 * p,
+            },
+        },
+    });
+    const Glass = () => ({
+        in: {
+            style: {
+                // `p` is number (0~1)
+                // When just before this page appear, `p` will be 0
+                // When this page filled your screen, `p` will be 1
+                transformOrigin: `right bottom`,
+                transform: (p) => {
+                    if (0.5 <= p) {
+                        return `rotate(${(p - 0.5) * 180}deg)`;
+                    } else {
+                        return `rotate(0deg)`;
+                    }
+                },
+            },
+        },
+        out: {
+            style: {
+                // `p` is number (0~1)
+                // When this page filled your screen, `p` will be 0
+                // When just after this page disappear, `p` will be 1
+                // transform: (p) => `rotate(${(50 + p * 40)}deg)`,
+                transformOrigin: `right bottom`,
 
-                transformOrigin: 'top center  ',
+                transform: (p) => `
+                 rotate (90deg)`,
             },
         },
     });
@@ -110,28 +157,20 @@ function Intro() {
     return (
         <>
             <div className="intro-container">
-                <div className="yun-fix-scroll">scroll down</div>
+                <div className="yun-fix-scroll">
+                    <p>SCROLL</p>
+                    <div className="yun-scroll-img">
+
+                    </div>
+                </div>
                 <div className="yun-intro-skip" onClick={backtoMainpage}>
-                    {' '}
-                    skip
+                    <p>SKIP</p>
+                    
                 </div>
 
                 <ScrollContainer>
+                    <BgStar />
                     <ScrollPage>
-                        {/* <div className="otherStars">
-                            <div className="otherStar"></div>
-                            <div className="otherStar"></div>
-                            <div className="otherStar"></div>
-                            <div className="otherStar"></div>
-                            <div className="otherStar"></div>
-                            <div className="otherStar"></div>
-                            <div className="otherStar"></div>
-                        </div> */}
-                        <Animator
-                            animation={batch(Sticky(50, 50), Fade(0.8, 1))}
-                        >
-                            <BgStar />
-                        </Animator>
                         <Animator animation={batch(Sticky(50, 50))}>
                             <div className="star"></div>
                         </Animator>
@@ -169,7 +208,11 @@ function Intro() {
                             <div className="starmask"></div>
                         </Animator>
                         <Animator
-                            animation={batch(Sticky(50, 50), ZoomOut(0, 5))}
+                            animation={batch(
+                                Sticky(50, 50),
+                                ZoomOut(0, 5),
+                                FadeIn(0, 1)
+                            )}
                         >
                             <div className="mainstar"></div>
                         </Animator>
@@ -180,34 +223,35 @@ function Intro() {
                                 MoveOut()
                             )}
                         >
-                            <h2>曾以為自己是萬中選一的璀璨星星</h2>
+                            <div className="intro-h2">
+                                曾以為自己是萬中選一的璀璨星星
+                            </div>
                         </Animator>
                     </ScrollPage>
 
-                    <ScrollPage>
-                        <Animator
-                            animation={batch(Sticky(50, 50), Fade(0.8, 1))}
-                        >
-                            <BgStar />
-                        </Animator>
-                    </ScrollPage>
+                    <ScrollPage></ScrollPage>
 
                     <ScrollPage>
-                        <Animator
-                            animation={batch(Sticky(50, 50), Fade(0.8, 1))}
-                        >
-                            <BgStar />
+                        <Animator animation={batch(Pos(50, 50, -50, 0))}>
+                            <div className="god-table"></div>
                         </Animator>
+
                         <Animator
                             animation={batch(
-                                Pos(50, 50, -25, -17),
-                                Push(),
-                                MoveIn()
+                                Pos(50, 50, 250, 30),
+                                MoveOut(30, 0),
+                                Glass()
                             )}
+                        >
+                            <div className="god-glass1"></div>
+                        </Animator>
+
+                        <Animator
+                            animation={batch(Pos(50, 50, 0, -25), Push())}
                         >
                             <div className="god-hand"></div>
                         </Animator>
-                        <Animator animation={batch(Sticky(50, 50), MoveIn())}>
+                        <Animator animation={batch(Sticky(50, 50))}>
                             <div className="god"></div>
                         </Animator>
                         <Animator animation={batch(Move(50, 50))}>
@@ -217,48 +261,42 @@ function Intro() {
                             animation={batch(
                                 Pos(8, 80, 0, 0),
                                 Fade(),
-                                MoveOut()
+                                Move(0, 50, 0, -50)
                             )}
                         >
-                            <h2>此刻卻懷疑自己是一個帶著瑕疵的作品</h2>
+                            <div className="intro-h2">
+                                此刻卻懷疑自己是一個帶著瑕疵的作品
+                            </div>
                         </Animator>
                     </ScrollPage>
+                    <ScrollPage></ScrollPage>
 
                     <ScrollPage>
                         <Animator
-                            animation={batch(Sticky(50, 50), Fade(0.8, 1))}
-                        >
-                            <BgStar />
-                        </Animator>
-                        <Animator
                             animation={batch(
-                                Pos(60, 30, 0, 0),
+                                Pos(40, 20, 0, 0),
                                 Fade(),
                                 MoveOut()
                             )}
                         >
-                            <Clock />
+                            <Clock2 />
                         </Animator>
                         <Animator
                             animation={batch(
-                                Pos(8, 80, 0, 0),
+                                Pos(8, 50, 0, 0),
                                 Fade(),
                                 MoveOut()
                             )}
                         >
-                            <h2>也許覺得自己生不逢時？</h2>
+                            <div className="intro-h2">
+                                也許覺得自己生不逢時？
+                            </div>
                         </Animator>
                     </ScrollPage>
-
                     <ScrollPage>
                         <Animator
-                            animation={batch(Sticky(50, 50), Fade(0.8, 1))}
-                        >
-                            <BgStar />
-                        </Animator>
-                        <Animator
                             animation={batch(
-                                Pos(60, 30, 0, 0),
+                                Pos(40, 25, 0, 0),
                                 Fade(),
                                 MoveOut()
                             )}
@@ -267,96 +305,115 @@ function Intro() {
                         </Animator>
                         <Animator
                             animation={batch(
-                                Pos(8, 80, 0, 0),
+                                Pos(8, 50, 0, 0),
                                 Fade(),
                                 MoveOut()
                             )}
                         >
-                            <h2>還是沒有誕生在適合自己的位置？</h2>
+                            <div className="intro-h2">
+                                還是沒有誕生在適合自己的位置？
+                            </div>
                         </Animator>
                     </ScrollPage>
+                    <ScrollPage></ScrollPage>
 
                     <ScrollPage>
                         <Animator
-                            animation={batch(Sticky(50, 50), Fade(0.8, 1))}
-                        >
-                            <BgStar />
-                        </Animator>
-                        <Animator
                             animation={batch(
-                                Pos(55, 15, 0, 0),
-                                Fade(),
-                                MoveOut()
+                                Pos(50, 10, 0, 0)
+                                // Fade(),
+                                // MoveOut()
                             )}
                         >
                             <div className="intro-box"></div>
                         </Animator>
                         <Animator
                             animation={batch(
-                                Pos(60, 30, 0, 0),
-                                Fade(),
-                                MoveOut()
+                                Pos(50, 10, 125, 190),
+                                // FadeIn(),
+                                MoveIn(100, 100)
                             )}
                         >
-                            <div className="Mirror2"></div>
+                            <div className="InsideMirror"></div>
                         </Animator>
                         <Animator
                             animation={batch(
-                                Pos(60, 30, 0, 0),
-                                Fade(),
-                                MoveOut()
+                                Pos(50, 10, 160, 640),
+                                FadeStay(),
+                                MoveIn(100, 100)
                             )}
                         >
-                            <div className="Mirror1"></div>
+                            <div className="mirrorEar"></div>
                         </Animator>
                         <Animator
                             animation={batch(
-                                Pos(8, 80, 0, 0),
-                                Fade(),
-                                MoveOut()
+                                Pos(50, 10, 340, 1460),
+                                // Fade(),
+                                Move(100, 100, 2, -4)
                             )}
                         >
-                            <h2>又或是不滿意</h2>
-                            <h2>自己與生俱來的樣子？</h2>
+                            <div className="mirrorEye"></div>
+                        </Animator>
+                        <Animator
+                            animation={batch(
+                                Pos(50, 10, 40, 40)
+                                // Fade(),
+                            )}
+                        >
+                            <div className="mirror1"></div>
+                        </Animator>
+                        <Animator
+                            animation={batch(
+                                Pos(50, 10, 110, 105),
+                                // Fade(),
+                                MoveIn(100, 100)
+                            )}
+                        >
+                            <div className="mirrorMan"></div>
+                        </Animator>
+
+                        <Animator
+                            animation={batch(
+                                Pos(8, 50, 0, 0),
+                                Fade()
+                                // MoveOut()
+                            )}
+                        >
+                            <div className="intro-h2">又或是不滿意</div>
+                            <div className="intro-h2">自己與生俱來的樣子？</div>
+                        </Animator>
+                        <Animator
+                            animation={batch(
+                                Pos(50, 10, 0, 0)
+                                // Fade(),
+                                // MoveOut()
+                            )}
+                        >
+                            <div className="intro-boxborder"></div>
                         </Animator>
                     </ScrollPage>
 
-                    <ScrollPage>
-                        <Animator
-                            animation={batch(Sticky(50, 50), Fade(0.8, 1))}
-                        >
-                            <BgStar />
-                        </Animator>
-                    </ScrollPage>
+                    <ScrollPage></ScrollPage>
 
                     <ScrollPage>
                         <Animator
-                            animation={batch(Sticky(50, 50), Fade(0.8, 1))}
-                        >
-                            <BgStar />
-                        </Animator>
-                        <Animator
-                            animation={batch(Pos(20, 30, 0, 0), FadeIn())}
+                            animation={batch(Pos(10, 10, 0, 0), FadeIn())}
                         >
                             <div className="intro-final-logo"></div>
                         </Animator>
                         <Animator
                             animation={batch(Pos(50, 40, 0, 0), FadeIn())}
                         >
-                            <h2>那麼即刻開始打造下一段人生吧</h2>
-                            <h2>你此生的遺憾，將於來生圓滿</h2>
-                            <h2>歡迎光臨 來生投放所</h2>
+                            <div className="intro-h2">
+                                即刻開始打造下一段人生吧
+                                <br />
+                                你此生的遺憾，將於來生圓滿 <br />
+                                歡迎光臨 來生投放所
+                            </div>
                         </Animator>
                     </ScrollPage>
 
-                    <ScrollPage>
-                        <Animator
-                            animation={batch(Sticky(50, 50), Fade(0.8, 1))}
-                        >
-                            <BgStar />
-                        </Animator>
-                        <div>END</div>
-                    </ScrollPage>
+                    <ScrollPage></ScrollPage>
                 </ScrollContainer>
             </div>
         </>
