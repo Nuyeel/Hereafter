bb1f212a1465322f1f57dc3b555e57b4d3bf8148
Abby <chiquzt@gmail.com>
2022-08-23 00:10:04 +0800

    鬼鬼

--- client/src/pages/AboutUsSecond/components/NameCard.jsx
+++ client/src/pages/AboutUsSecond/components/NameCard.jsx
@@ -1,13 +1,25 @@
 import { ReactComponent as Ghost } from '../../AboutUsThird/imgs/ghost.svg';
 function NameCard(props) {
+    const getRandom = (min, max) => {
+        return Math.floor(Math.random() * max) + min;
+    };
+    const random = getRandom(0, 2);
+
     return (
         <>
             <div className="ab-box">
                 <div className="ab-g-jump">
-                    <Ghost className="ab-g-1" />
-                    <Ghost className="ab-g-2" />
-                    <Ghost className="ab-g-3" />
-                    {/* <Ghost /> */}
+                    {/* 分開亂數 */}
+                    <Ghost className={'ab-g-a' + getRandom(0, 2)} />
+                    <Ghost className={'ab-g-b' + getRandom(0, 2)} />
+                    <Ghost className={'ab-g-c' + getRandom(0, 2)} />
+                    <Ghost className={'ab-g-a' + getRandom(0, 2)} />
+                    <Ghost className={'ab-g-b' + getRandom(0, 2)} />
+                    <Ghost className={'ab-g-c' + getRandom(0, 2)} />
+                    {/* 一組一起變 */}
+                    {/* <Ghost className={'ab-g-a' + random} />
+                    <Ghost className={'ab-g-b' + random} />
+                    <Ghost className={'ab-g-c' + random} /> */}
                 </div>
                 <div className="ab-pic">
                     <img src={'images/aboutus/' + props.card.pic} alt=""></img>
